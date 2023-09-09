e538cf6020f7b85fe3b4d4a4f1beb30c7fb63f18
Matias <matiasbenincasa@xpertai.com>
2023-09-09 15:07:23 -0300

    Show card skeleton when loading

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -19,23 +19,27 @@ interface GamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const controller = new AbortController();
     client
       .get<GamesResponse>('/games', { signal: controller.signal })
       .then((response) => {
         setGames(response.data.results);
+        setIsLoading(false);
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
