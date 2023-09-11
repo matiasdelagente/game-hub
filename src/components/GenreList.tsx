8b1b2ae21901a4b1c5685929ab521b4a60ece6ef
Matias <matiasbenincasa@xpertai.com>
2023-09-11 10:07:51 -0300

    Highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -24,6 +25,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
             <Button
               variant="link"
               fontSize="lg"
+              fontWeight={selectedGenre?.id === genre.id ? 'bold' : 'normal'}
               onClick={() => onSelectGenre(genre)}
             >
               {genre.name}
