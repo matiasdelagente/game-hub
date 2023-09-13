82e894dba5967589ed2195b376757743cb868605
Matias <matiasbenincasa@xpertai.com>
2023-09-12 23:17:51 -0300

    rafactor game grid

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -13,29 +13,22 @@ const GamesGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, , 3, 4, 6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <ul>
-        <SimpleGrid
-          columns={{ sm: 2, md: 2, lg: 3, xl: 4 }}
-          spacing={6}
-          padding="10px"
-        >
-          {isLoading &&
-            skeletons.map((skeleton) => (
-              <GameCardContainer key={skeleton}>
-                <GameCardSkeleton />
-              </GameCardContainer>
-            ))}
-          {data.map((game) => (
-            <GameCardContainer key={game.id}>
-              <GameCard game={game} />
-            </GameCardContainer>
-          ))}
-        </SimpleGrid>
-      </ul>
-    </>
+    <SimpleGrid columns={{ sm: 2, md: 2, lg: 3, xl: 4 }} spacing={6} padding="10px">
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
